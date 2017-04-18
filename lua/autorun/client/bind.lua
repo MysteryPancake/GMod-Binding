0183334959f42ea29b42ec42304eb77d2bd082c3
MysteryPancake <Mysterypancake1@users.noreply.github.com>
2017-04-18 16:12:28 +1000

    Made it a module!

--- lua/autorun/client/bind.lua
+++ lua/autorun/client/bind.lua
@@ -1,31 +1,39 @@
-bind = { Bindings = {} }
+local hook = hook
+local input = input
+local pairs = pairs
+local isfunction = isfunction
+local isnumber = isnumber
 
-function bind.Add( key, name, func )
+module( "bind" )
+
+local Bindings = {}
+
+function Add( key, name, func )
 
 	if !isfunction( func ) then return end
 	if !isnumber( key ) then return end
 
-	if bind.Bindings[ key ] == nil then
-		bind.Bindings[ key ] = {}
+	if Bindings[ key ] == nil then
+		Bindings[ key ] = {}
 	end
 
-	bind.Bindings[ key ][ name ] = func
+	Bindings[ key ][ name ] = func
 
 end
 
-function bind.Remove( key, name )
+function Remove( key, name )
 
 	if !isnumber( key ) then return end
-	if !bind.Bindings[ key ] then return end
+	if !Bindings[ key ] then return end
 
-	bind.Bindings[ key ][ name ] = nil
+	Bindings[ key ][ name ] = nil
 
 end
 
 local FirstPressed = {}
 
 hook.Add( "Tick", "CallBindings", function()
-	for key, tbl in pairs( bind.Bindings ) do
+	for key, tbl in pairs( Bindings ) do
 		if input.IsButtonDown( key ) then
 			if FirstPressed[ key ] then
 				for _, func in pairs( tbl ) do func() end
