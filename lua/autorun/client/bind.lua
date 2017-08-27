b4dc062e20d4a4952e238bdb380ced5e150b5727
Mystery Pancake <MysteryPancake@users.noreply.github.com>
2017-08-27 11:33:04 +1000

    Module is deprecated :(

--- lua/autorun/client/bind.lua
+++ lua/autorun/client/bind.lua
@@ -1,24 +1,18 @@
-local hook = hook
-local input = input
-local pairs = pairs
-local isfunction = isfunction
-local isnumber = isnumber
-
-module( "bind" )
+bind = {}
 
 local Bindings = {}
 
 --[[---------------------------------------------------------
-    bind.GetTable()
+    GetTable()
     Returns a table of all bindings.
 -----------------------------------------------------------]]
-function GetTable() return Bindings end
+function bind.GetTable() return Bindings end
 
 --[[---------------------------------------------------------
-    bind.Add( number button, any identifier, function func )
+    Add( number button, any identifier, function func )
     Add a binding to run when the button is pressed.
 -----------------------------------------------------------]]
-function Add( btn, name, func )
+function bind.Add( btn, name, func )
 
 	if !isfunction( func ) then return end
 	if !isnumber( btn ) then return end
@@ -32,10 +26,10 @@ function Add( btn, name, func )
 end
 
 --[[---------------------------------------------------------
-    bind.Remove( number button, any identifier )
+    Remove( number button, any identifier )
     Removes the binding with the given identifier.
 -----------------------------------------------------------]]
-function Remove( btn, name )
+function bind.Remove( btn, name )
 
 	if !isnumber( btn ) then return end
 	if !Bindings[ btn ] then return end
